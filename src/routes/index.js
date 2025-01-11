2e84f7ba426f05d7aff389a6e4850d92102b4524
msuryasyahruli <msuryasyahruli12@gmail.com>
2025-01-12 00:25:56 +0700

    update router

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,17 @@ const router = express.Router();
 const pickupRequestRouter = require("./pickupRequest");
 const usersRouter = require("../routes/users");
 
+router.get("/", (req, res) => {
+  res.send(`
+      <h1>MAKA Project Rest API</h1>
+      <p>Endpoint yang tersedia:</p>
+      <ul>
+        <li><a href="/pickup-request">/pickup-request</a></li>
+        <li><a href="/users">/users</a></li>
+      </ul>
+    `);
+});
+
 router.use("/pickup-request", pickupRequestRouter);
 router.use("/users", usersRouter);
 
